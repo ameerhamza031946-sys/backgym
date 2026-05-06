cf544558114b2af47b69a723de8b5a99a33be4c4
ok <ok@example.com>
2026-05-06 18:18:16 +0500

    Fix missing Depends import

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorDatabase
 import time
